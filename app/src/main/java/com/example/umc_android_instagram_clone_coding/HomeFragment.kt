2d4a8e6e2bcc3258727eaf9b13ced3e6d4a65c48
JM2308 <wlals2987@naver.com>
2022-05-30 03:10:16 +0900

    Add fragment_home.xml and notice_home.xml

---
+++ app/src/main/java/com/example/umc_android_instagram_clone_coding/HomeFragment.kt
@@ -0,0 +1,4 @@
+package com.example.umc_android_instagram_clone_coding
+
+class HomeFragment {
+}
--- app/src/main/java/com/example/umc_android_instagram_clone_coding/MainActivity.kt
+++ app/src/main/java/com/example/umc_android_instagram_clone_coding/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.umc_android_instagram_clone_coding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.umc_android_instagram_clone_coding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
     }
 }
