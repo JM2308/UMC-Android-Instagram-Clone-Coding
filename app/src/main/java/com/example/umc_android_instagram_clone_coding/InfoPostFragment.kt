6164858fefbd46560ebe0fc2b23b7b0f34c69a6d
lsh9295 <lsh929500@gmail.com>
2022-06-04 19:06:19 +0900

    FEAT: [InfoPostFragment] RecyclerView(GridLayout)

--- app/src/main/java/com/example/umc_android_instagram_clone_coding/InfoPostFragment.kt
+++ app/src/main/java/com/example/umc_android_instagram_clone_coding/InfoPostFragment.kt
@@ -5,10 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.umc_android_instagram_clone_coding.databinding.FragmentInfoPostBinding
 
 class InfoPostFragment : Fragment() {
     lateinit var binding: FragmentInfoPostBinding
+    private var postItems = arrayListOf<PostItem>(
+        PostItem(R.drawable.jindol),
+        PostItem(R.drawable.jindol),
+        PostItem(R.drawable.jindol),
+        PostItem(R.drawable.jindol),
+        PostItem(R.drawable.jindol),
+        PostItem(R.drawable.jindol)
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +25,12 @@ class InfoPostFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentInfoPostBinding.inflate(inflater, container, false)
+
+        val mAdapter = InfoPostRVAdapter(postItems)
+        binding.infoPostRv.adapter = mAdapter
+
+        val gridLayoutManager = GridLayoutManager(context,3)
+        binding.infoPostRv.layoutManager = gridLayoutManager
 
         return binding.root
     }
