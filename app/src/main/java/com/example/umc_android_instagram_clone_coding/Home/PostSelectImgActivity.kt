0463483389c00a7d758f21ba52321edc8d743629
JM2308 <wlals2987@naver.com>
2022-06-25 16:00:20 +0900

    Add Writing Post Activity Ver. 2

--- app/src/main/java/com/example/umc_android_instagram_clone_coding/Home/PostSelectImgActivity.kt
+++ app/src/main/java/com/example/umc_android_instagram_clone_coding/Home/PostSelectImgActivity.kt
@@ -1,25 +1,24 @@
 package com.example.umc_android_instagram_clone_coding.Home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.umc_android_instagram_clone_coding.Data.PostSelectImg
-import com.example.umc_android_instagram_clone_coding.Info.InfoPostRVAdapter
 import com.example.umc_android_instagram_clone_coding.R
 import com.example.umc_android_instagram_clone_coding.databinding.ActivityPostSelectImgBinding
 
 class PostSelectImgActivity: AppCompatActivity() {
     lateinit var binding: ActivityPostSelectImgBinding
     private var imgDatas = ArrayList<PostSelectImg>()
+    private var selectPosition: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityPostSelectImgBinding.inflate(layoutInflater)
 
-        // 데이터 리스트 생성 더미 데이터 (arraylist에 담길 데이터)
         imgDatas.apply {
             add(PostSelectImg(R.drawable.img_shop1))
             add(PostSelectImg(R.drawable.img_shop2))
@@ -33,7 +32,26 @@ class PostSelectImgActivity: AppCompatActivity() {
 
         val postSelectAdapter = PostSelectImgRVAdapter(imgDatas) // 더미데이터랑 Adapter 연결
         binding.postSelectImgRv.adapter = postSelectAdapter // 리사이클러뷰에 어댑터를 연결
-        binding.postSelectImgRv.layoutManager = GridLayoutManager(this,3)
+
+        postSelectAdapter.setMyItemClickListener(object : PostSelectImgRVAdapter.ItemClickListener{
+            override fun getClickItem(img: PostSelectImg, position: Int) {
+                // 상단 ImageView에 선택한 이미지 띄우기
+                binding.postSelectImg.setImageResource(img.img!!)
+
+                // 선택 해제된 이미지 투명도 상태 원래대로
+                if (selectPosition != null)
+                    binding.postSelectImgRv[selectPosition!!].alpha = 1F
+
+                // 새로 선택한 이미지 투명도 적용
+                binding.postSelectImgRv[position].alpha = 0.5F
+                selectPosition = position
+
+                Log.d("ItemClickCheck", "Activity Item Click Check")
+                Log.d("ItemClickCheck", "Click Img = " + binding.postSelectImgRv.get(position))
+            }
+        })
+
+        binding.postSelectImgRv.layoutManager = GridLayoutManager(this,4)
 
         setContentView(binding.root)
     }
--- app/src/main/java/com/example/umc_android_instagram_clone_coding/Home/PostSelectImgRVAdapter.kt
+++ app/src/main/java/com/example/umc_android_instagram_clone_coding/Home/PostSelectImgRVAdapter.kt
@@ -1,15 +1,28 @@
 package com.example.umc_android_instagram_clone_coding.Home
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
 import androidx.recyclerview.widget.RecyclerView
 import com.example.umc_android_instagram_clone_coding.Data.PostSelectImg
-import com.example.umc_android_instagram_clone_coding.R
 import com.example.umc_android_instagram_clone_coding.databinding.ItemPostImgSelectBinding
 
 class PostSelectImgRVAdapter(private var imgList: ArrayList<PostSelectImg>) : RecyclerView.Adapter<PostSelectImgRVAdapter.ViewHolder>() {
+
+
+    interface ItemClickListener {
+        fun getClickItem(img: PostSelectImg, position: Int)
+    }
+
+    // 리스너 객체를 전달받는 함수랑 리스너 객체를 저장할 변수
+    private lateinit var mItemClickListener: ItemClickListener
+
+    fun setMyItemClickListener(itemClickListener: ItemClickListener){
+        mItemClickListener = itemClickListener
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -21,6 +34,16 @@ class PostSelectImgRVAdapter(private var imgList: ArrayList<PostSelectImg>) : Re
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(imgList[position])
+
+        holder.itemView.setOnClickListener {
+            // val intent = Intent(holder.itemView?.context, PostSelectImgActivity::class.java)
+            // intent.putExtra("position", position)
+            mItemClickListener.getClickItem(imgList[position], position)
+            Log.d("ItemClickCheck", "position = $position")
+
+            // imgList[position].img!!
+            // ContextCompat.startActivity(holder.itemView.context, intent, null)
+        }
     }
 
     override fun getItemCount(): Int = imgList.size
@@ -28,13 +51,11 @@ class PostSelectImgRVAdapter(private var imgList: ArrayList<PostSelectImg>) : Re
     inner class ViewHolder(val binding: ItemPostImgSelectBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind (img: PostSelectImg) {
             binding.postImg.setImageResource(img.img!!)
-
-            binding.postImg.setOnClickListener {
-                Intent(context, PostSelectImgActivity::class.java).apply {
-                    putExtra("selectImg", position)
-
-                }
-            }
         }
     }
+
+    fun checkItemPosition(position: Int): Int {
+        return position
+    }
+
 }
